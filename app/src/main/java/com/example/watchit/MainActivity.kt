bd36ff2061833f9121075644af4f433afa7943e1
gil468 <gil123991gmail.com>
2023-12-29 18:02:07 +0200

    Add login page on activity_main.xml

--- app/src/main/java/com/example/watchit/MainActivity.kt
+++ app/src/main/java/com/example/watchit/MainActivity.kt
@@ -15,14 +15,7 @@ import com.example.watchit.ui.theme.WatchItTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            WatchItTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main);
     }
 }
 
