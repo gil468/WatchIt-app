3d7ab945867af7cf6d4b30e7146b218fa3175bbe
Oren <oren.hta@gmail.com>
2024-01-14 21:49:41 +0200

    remove commented code

--- app/src/main/java/com/example/watchit/MovieActivity.kt
+++ app/src/main/java/com/example/watchit/MovieActivity.kt
@@ -20,24 +20,7 @@ class MovieActivity : ComponentActivity() {
         defineMovie()
         loadMovieDetails()
         addAddReviewOnClickHandler()
-        //fetchReviews()
     }
-
-//    private fun fetchReviews() {
-//        Firebase.firestore.collection("reviews")
-//            .whereEqualTo("movieId", movie.id)
-//            .orderBy("date", Query.Direction.DESCENDING)
-//            .get()
-//            .addOnSuccessListener(::showMovieReviews)
-//            .addOnFailureListener {
-//                Log.e("reviews", it.message, it)
-//                Toast.makeText(
-//                    this@MovieActivity,
-//                    "Error processing reviews",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//            }
-//    }
 
     private fun addAddReviewOnClickHandler() {
         findViewById<Button>(R.id.addReview).setOnClickListener {
@@ -54,59 +37,6 @@ class MovieActivity : ComponentActivity() {
             intent.getSerializableExtra("movie") as Movie
         }
     }
-
-//    private fun showMovieReviews(reviews: QuerySnapshot) {
-//        val reviewsLayout: LinearLayout = findViewById(R.id.reviewsLayout)
-//
-//        for (reviewDocument in reviews) {
-//            val review = reviewDocument.toObject<PublishReviewDTO>()
-//
-//            val layout = LinearLayout(this)
-//            layout.orientation = LinearLayout.VERTICAL
-//            val imageView = ImageView(this)
-//
-//            Firebase.storage.reference.child("images/reviews/${reviewDocument.id}")
-//                .downloadUrl
-//                .addOnSuccessListener {
-//                    Picasso.get().load(it).into(imageView)
-//                }.addOnFailureListener {
-//                    //todo
-//                }
-//
-//            val layoutParams = LinearLayout.LayoutParams(800, 250)
-//                .apply {
-//                    weight = 1.0f
-//                    gravity = Gravity.CENTER
-//                }
-//            layoutParams.topMargin = 50
-//
-//            imageView.layoutParams = layoutParams
-//
-//            val reviewText = TextView(this)
-//            val params = LinearLayout.LayoutParams(
-//                LinearLayout.LayoutParams.WRAP_CONTENT,
-//                LinearLayout.LayoutParams.WRAP_CONTENT
-//            ).apply {
-//                weight = 1.0f
-//                gravity = Gravity.CENTER
-//            }
-//            reviewText.textSize = 20f
-//            reviewText.text = review.text
-//            reviewText.layoutParams = params
-//
-//            val score = TextView(this)
-//            score.textSize = 20f
-//            score.text = getString(R.string.rating, review.score)
-//            score.layoutParams = params
-//
-//
-//            layout.addView(imageView)
-//            layout.addView(reviewText)
-//            layout.addView(score)
-//
-//            reviewsLayout.addView(layout)
-//        }
-//    }
 
     private fun loadMovieDetails() {
         val movieTitle: TextView = findViewById(R.id.movieTitle)
